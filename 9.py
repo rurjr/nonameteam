b4299d234c35a393e863b1f188b68cb7d1e67f3c
SofiaMikhailovaa <146183150+SofiaMikhailovaa@users.noreply.github.com>
2023-12-10 11:53:54 +0200

    Delete 9.py

--- 9.py
+++
@@ -1,8 +0,0 @@
-angle = float(input('enter angle: '))
-def equal_angle(angle):
-    if angle > 0:
-        angle = angle
-    else:
-        angle = 360 - abs(angle)
-    print(int(angle))
-equal_angle(angle)
